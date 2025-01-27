41c010fb511b9752542f404601d1ccb43517a0ba
laraibi.mohammadjavad.etu <laraibi.mohammadjavad.etu@univ-lille.fr>
2025-01-27 11:07:37 +0100

    ajouter les tests unitaires

--- src/webinars/adapters/webinar-repository.prisma.int.test.ts
+++ src/webinars/adapters/webinar-repository.prisma.int.test.ts
@@ -1,2 +1,81 @@
 // Test d'intégration
 // C. Ecriture de notre premier test d'intégration
+
+import { PrismaClient } from '@prisma/client';
+import {
+  PostgreSqlContainer,
+  StartedPostgreSqlContainer,
+} from '@testcontainers/postgresql';
+import { exec } from 'child_process';
+import { PrismaWebinarRepository } from 'src/webinars/adapters/webinar-repository.prisma';
+import { Webinar } from 'src/webinars/entities/webinar.entity';
+import { promisify } from 'util';
+const asyncExec = promisify(exec);
+
+describe('PrismaWebinarRepository', () => {
+  let container: StartedPostgreSqlContainer;
+  let prismaClient: PrismaClient;
+  let repository: PrismaWebinarRepository;
+  
+  beforeAll(async () => {
+    // Connect to database
+    container = await new PostgreSqlContainer()
+      .withDatabase('test_db')
+      .withUsername('user_test')
+      .withPassword('password_test')
+      .withExposedPorts(5432)
+      .start();
+  
+    const dbUrl = container.getConnectionUri();
+    prismaClient = new PrismaClient({
+      datasources: {
+        db: { url: dbUrl },
+      },
+    });
+  
+    // Run migrations to populate the database
+    await asyncExec(`DATABASE_URL=${dbUrl} npx prisma migrate deploy`);
+  
+    return prismaClient.$connect();
+  });
+
+  beforeEach(async () => {
+    repository = new PrismaWebinarRepository(prismaClient);
+    await prismaClient.webinar.deleteMany();
+  });
+
+  describe('Scenario : repository.create', () => {
+    it('should create a webinar', async () => {
+      // ARRANGE
+      const webinar = new Webinar({
+        id: 'webinar-id',
+        organizerId: 'organizer-id',
+        title: 'Webinar title',
+        startDate: new Date('2022-01-01T00:00:00Z'),
+        endDate: new Date('2022-01-01T01:00:00Z'),
+        seats: 100,
+      });
+  
+      // ACT
+      await repository.create(webinar);
+  
+      // ASSERT
+      const maybeWebinar = await prismaClient.webinar.findUnique({
+        where: { id: 'webinar-id' },
+      });
+      expect(maybeWebinar).toEqual({
+        id: 'webinar-id',
+        organizerId: 'organizer-id',
+        title: 'Webinar title',
+        startDate: new Date('2022-01-01T00:00:00Z'),
+        endDate: new Date('2022-01-01T01:00:00Z'),
+        seats: 100,
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await prismaClient.$disconnect();
+    await container.stop({ timeout: 1000 });
+  });
+});
--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,132 @@
 // Tests unitaires
 
+import { testUser } from "src/users/tests/user-seeds";
+import { ChangeSeats } from "src/webinars/use-cases/change-seats";
+import { IWebinarRepository } from "src/webinars/ports/webinar-repository.interface";
+import { Webinar } from "../entities/webinar.entity";
+import { InMemoryWebinarRepository } from "../adapters/webinar-repository.in-memory";
 
+// Test unitaire
 describe('Feature : Change seats', () => {
-  // Initialisation de nos tests, boilerplates...
+  // Initialisation des variables
+  let webinarRepository: IWebinarRepository;
+  let useCase: ChangeSeats;
+  
+  const webinar = new Webinar({
+    id: 'webinar-id',
+    organizerId: testUser.alice.props.id,
+    title: 'Webinar title',
+    startDate: new Date('2024-01-01T00:00:00Z'),
+    endDate: new Date('2024-01-01T01:00:00Z'),
+    seats: 200,
+  });
+
+  // Initialisation des données
+  beforeEach(() => {
+    webinarRepository = new InMemoryWebinarRepository([webinar]);
+    useCase = new ChangeSeats(webinarRepository);
+  });
+
+  async function whenUserChangeSeatsWith(payload:any) {
+    // Act
+    return useCase.execute(payload);
+  }
+
+  async function thenUpdatedWebinarSeatsShouldBe(seats: number) {
+    // Assert
+    const updatedWebinar = await webinarRepository.findById('webinar-id');
+    expect(updatedWebinar?.props.seats).toEqual(seats);
+  }
+    
+  // Fonctionnalité à tester
   describe('Scenario: Happy path', () => {
-    // Code commun à notre scénario : payload...
+    
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 300,
+    };
+
     it('should change the number of seats for a webinar', async () => {
+      // Act
+      await whenUserChangeSeatsWith(payload);
+      // Assert
+      await thenUpdatedWebinarSeatsShouldBe(300);
+    });
+  });
+
+  describe('Scenario: Webinar does not exist', () => {
+    // Code commun à notre scénario : payload...
+    let payload = {
+      user: testUser.alice,
+      webinarId: 'id',
+      seats: 100,
+    };
+
+    it('should throw an error if the webinar does not exist', async () => {
      // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+      await expect(whenUserChangeSeatsWith(payload)).rejects.toThrowError('Webinar not found');
+    });
+
+    it('should verify the webinar seats', async () => {
+      await thenUpdatedWebinarSeatsShouldBe(200);
+    });
+  });
+
+  describe('Scenario: Update the webinar of someone else', () => {
+    // Code commun à notre scénario : payload...
+    let payload = {
+      user: testUser.bob,
+      webinarId: 'webinar-id',
+      seats: 100,
+    };
+
+    it('should throw an error if the user is not the organizer', async () => {
+     // Vérification de la règle métier, condition testée...
+      await expect(whenUserChangeSeatsWith(payload)).rejects.toThrowError('User is not allowed to update this webinar');
+    });
+
+    it('should verify the webinar seats', async () => {
+      await thenUpdatedWebinarSeatsShouldBe(200);
+    });
+  });
+  
+  describe('Scenario: Change seat to an inferior number', () => {
+
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 50,
+    };
+
+    it('should throw an error if the number of seats is inferior to the current number of participants', async () => {
+      //act
+      await expect(whenUserChangeSeatsWith(payload)).rejects.toThrowError('You cannot reduce the number of seats');
+    });
+
+    it('should verify the webinar seats', async () => {
+      await thenUpdatedWebinarSeatsShouldBe(200);
+    });
+  });
+
+  describe('Scenario:change seat to a number > 1000', () => {
+
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 1001,
+    };
+
+
+    it('should throw an error if the number of seats is superior to 1000', async () => {
+      //act
+      await expect(whenUserChangeSeatsWith(payload)).rejects.toThrowError('Webinar must have at most 1000 seats');
+    });
+
+    it('should verify the webinar seats', async () => {
+      await thenUpdatedWebinarSeatsShouldBe(200);
     });
   });
 });
+
+
